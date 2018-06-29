4f59ff13d5f0b14b8d7f55b2553563b1644cc26a
Thomas Wesley Matlock <thomasmatlock1994@gmail.com>
2018-06-29 23:42:07 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -26,12 +26,6 @@ class Student
 end
 
 def save
-    sql = <<-SQL
-      INSERT INTO students (name, grade)
-      VALUES (?, ?)
-    SQL
-
-    DB[:conn].execute(sql, self.name, self.grade)
-    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+    
 end
 end
