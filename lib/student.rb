c0063cdc1277d589c23c625a3407a60f1da3ce48
Thomas Wesley Matlock <thomasmatlock1994@gmail.com>
2018-06-29 23:50:50 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -57,7 +57,7 @@ def save
   def self.find_by_name(name)
     sql = "SELECT * FROM students WHERE name = ?"
     result = DB[:conn].execute(sql, name)[0]
-    
+    Student.new(result[0], result[1], result[2])
   end
     
     def update
