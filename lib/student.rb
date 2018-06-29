d04bc61a11f2ccf5641f49fc4e05f8a8ab681b0b
Thomas Wesley Matlock <thomasmatlock1994@gmail.com>
2018-06-29 23:40:52 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,8 +1,8 @@
 require_relative "../config/environment.rb"
 
 class Student 
-  attr_accessor :id, :name, :grade
-  
+  attr_accessor :name, :grade
+  attr_reader :id
   def initialize(id=nil, name, grade)
     @id = id
     @name = name
