34fdbd7fab220bdbf6093bf34d47b9522157c4fc
Thomas Wesley Matlock <thomasmatlock1994@gmail.com>
2018-06-29 23:46:31 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -37,7 +37,10 @@ def save
       DB[:conn].execute(sql, self.name, self.grade)
       @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
       
+    end
+  end
+  
+  
       student.save
       student
-end
-end
+
