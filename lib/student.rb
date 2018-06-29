bc6325abde5622fc09375ba25ccbfd1bf37605ed
Thomas Wesley Matlock <thomasmatlock1994@gmail.com>
2018-06-29 23:53:33 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -62,7 +62,7 @@ class Student
   
 end
     
-  def update
+  def self.update
     sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
     DB[:conn].execute(sql, self.name, self.grade, self.id)
   end
